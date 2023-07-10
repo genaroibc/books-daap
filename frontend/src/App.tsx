1d6bb2fdf7ce9e4d1666f9bba4877c84d91d2071
genaroibc <bonavitaismael@gmail.com>
2023-07-10 20:51:53 -0300

    [FEAT]: display books

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,39 @@
 import "./App.css"
+import "@truffle/contract/browser-dist/truffle-contract.min.js"
+import { BookData } from "./types"
+import { useEffect, useState } from "react"
+import { BookCard } from "./components/shared/BookCard"
+import { getBooks } from "./services/getBooks"
 
 function App() {
-  return <h1>Books Dapp</h1>
+  const [books, setBooks] = useState<BookData[]>([])
+
+  useEffect(() => {
+    handleGetBooks()
+  }, [])
+
+  const handleGetBooks = () => getBooks().then(books => setBooks(books))
+
+  if (!window.ethereum) {
+    return <h1>Metamask not detected</h1>
+  }
+
+  return (
+    <main>
+      <h1>Books Dapp</h1>
+
+      <div className="flex flex-col gap-4">
+        {books.map(({ author, description, title }) => (
+          <BookCard
+            key={title}
+            author={author}
+            description={description}
+            title={title}
+          />
+        ))}
+      </div>
+    </main>
+  )
 }
 
 export default App
