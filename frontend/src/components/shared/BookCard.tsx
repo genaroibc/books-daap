48ffc37532c2149dea3b9a4811a2a7b33337de98
genaroibc <bonavitaismael@gmail.com>
2023-07-10 20:49:39 -0300

    [FEAT]: add shared components

---
+++ frontend/src/components/shared/BookCard.tsx
@@ -0,0 +1,24 @@
+import { BookData } from "../../types"
+
+export function BookCard({ author, description, title }: BookData) {
+  return (
+    <article className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+      <img
+        className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        src="https://picsum.photos/300"
+        alt=""
+      />
+      <div className="flex flex-col justify-between p-4 leading-normal">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {title}
+        </h5>
+        <h6 className="mb-2 text-xl font-semibold tracking-tight text-gray-800 dark:text-white">
+          {author}
+        </h6>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {description}
+        </p>
+      </div>
+    </article>
+  )
+}
