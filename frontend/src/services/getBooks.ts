abc8f29e3e021ad8a62d50e853c8fa642d177295
genaroibc <bonavitaismael@gmail.com>
2023-07-10 20:49:08 -0300

    [DEV]: add service to get books

---
+++ frontend/src/services/getBooks.ts
@@ -0,0 +1,21 @@
+import { deployedBooksContract } from "../lib/bookContract"
+import { BookData } from "../types"
+
+export async function getBooks(): Promise<BookData[]> {
+  const booksCount = Number(await deployedBooksContract.booksCount())
+
+  const booksPromises = new Array(Number(booksCount))
+    .fill(0)
+    .map((_, index) => {
+      return deployedBooksContract.books(index) as Promise<BookData>
+    })
+
+  const books = (await Promise.allSettled(booksPromises)).map(
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    ({ value }) => value
+  )
+
+  return books as BookData[]
+}
