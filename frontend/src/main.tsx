97671e27be0692bba27dc0ffce6f83facb2663f2
genaroibc <bonavitaismael@gmail.com>
2023-07-10 20:43:58 -0300

    [DEV]: add declaration for global modules

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
+
+declare global {
+  interface Window {
+    TruffleContract: any
+    ethereum:
+      | {
+          request: (args: { method: string; params?: any[] }) => Promise<any>
+        }
+      | undefined
+  }
+}
