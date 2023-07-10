1f2b2ebb2e6d684904f8c6b5d06e13577b9d4214
genaroibc <bonavitaismael@gmail.com>
2023-07-10 20:52:38 -0300

    [FEAT]: let users create books

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css"
 import "@truffle/contract/browser-dist/truffle-contract.min.js"
+import { CreateBookForm } from "./components/CreateBookForm"
 import { BookData } from "./types"
 import { useEffect, useState } from "react"
 import { BookCard } from "./components/shared/BookCard"
 import { getBooks } from "./services/getBooks"
+import { createBook } from "./services/createBook"
 
 function App() {
   const [books, setBooks] = useState<BookData[]>([])
@@ -18,9 +20,19 @@ function App() {
     return <h1>Metamask not detected</h1>
   }
 
+  const handleCreateBook = (bookData: BookData) => {
+    createBook(bookData).then(() => {
+      setTimeout(() => {
+        handleGetBooks()
+      }, 1000)
+    })
+  }
+
   return (
     <main>
       <h1>Books Dapp</h1>
+
+      <CreateBookForm onSubmit={handleCreateBook} />
 
       <div className="flex flex-col gap-4">
         {books.map(({ author, description, title }) => (
